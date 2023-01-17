113a13364c56a3fec76ab1a049ffe668428230ca
Mike Wheelmakersson <radumihairotariu@gmail.com>
2023-01-17 15:11:47 +0200

    Update automatic_translator.py
    Finalized automatic translation script.

--- recipes_index/automatic_translator.py
+++ recipes_index/automatic_translator.py
@@ -4,33 +4,91 @@
 #   This script generates the first rough translation of the recipes,
 #       which will be improved by volunteer translators using pull requests to this repository
 
-# First install the googletrans package using the command
-#   pip install googletrans
-import googletrans as gt
+from google.cloud import translate
+from google.cloud import translate_v2 as tv2
 import os
 
 # Check if folder for the language exists, if not create it
-def checkFolder(language):
-    if not os.path.exists(language):
-        os.makedirs(language)
+def checkFolders(availableLanguages):
+    # Iterate over the available languages
+    for language in availableLanguages:
+        # print(availableLanguages[language])
 
-# Main entry point of the script
-def __main__():
-    # Get the list of available languages and save in local variable
-    availableLanguages = gt.LANGUAGES
-    # print(availableLanguages)
+        # Get parent directory of script
+        parentDir = os.path.dirname(os.path.realpath(__file__))
+        # print(parentDir)
+        folderPath = os.path.join(parentDir, language['name'])
+        # print(folderPath)
+        if not os.path.exists(folderPath):
+            os.makedirs(folderPath)
+
+# Translate the recipe and save it in the destination language folder
+def translateRecipe(recipe, originLanguage, destinationLanguage, originLanguageCode, destinationLanguageCode):
+    # Make v3 client
+    client = translate.TranslationServiceClient()
 
     # Get parent directory of script
     parentDir = os.path.dirname(os.path.realpath(__file__))
     # print(parentDir)
+    originFolderPath = os.path.join(parentDir, originLanguage)
+    # print(originFolderPath)
+    destinationFolderPath = os.path.join(parentDir, destinationLanguage)
+    # print(destinationFolderPath)
 
-    # Iterate over the available languages
+    # Open the recipe file
+    with open(os.path.join(originFolderPath, recipe), 'r', encoding='utf-8') as f:
+        """
+        # Translating the whole recipe at once
+        # Read the file
+        recipeText = f.read()
+        # print(recipeText)
+
+        # Translate the recipe
+        translatedRecipe = client.translate_text(parent=f"projects/freeopensourcerecipes/locations/global", contents=recipeText, source_language_code=originLanguageCode, target_language_code=destinationLanguageCode)
+        for translation in translatedRecipe.translations:
+            print("Translation: {}".format(translation.translated_text))
+        """
+
+        # Translating the recipe line by line
+        # Read the file line by line
+        recipeLines = f.readlines()
+        # print(recipeLines)
+
+        # Translate the recipe line by line
+        translatedRecipe = client.translate_text(parent=f"projects/freeopensourcerecipes/locations/global", contents=recipeLines, mime_type=f"text/plain",source_language_code=originLanguageCode, target_language_code=destinationLanguageCode)
+        # for translation in translatedRecipe.translations:
+            # print("Translation: {}".format(translation.translated_text))
+    
+    
+    # Save the translated recipe in the destination language folder
+    with open(os.path.join(destinationFolderPath, recipe), 'w', encoding='utf-8') as f:
+        for translation in translatedRecipe.translations:
+            f.write(translation.translated_text)
+
+
+# Main entry point of the script
+def main(recipe, originLanguage):
+    oldClient = tv2.Client()
+
+    # Get the list of available languages and save in local variable
+    print("Getting available languages...")
+    availableLanguages = oldClient.get_languages()
+    # print(availableLanguages)
+
+    # Check if the folders for the other languages exist, if not create them
+    print("Checking if folders for other languages exist...")
+    checkFolders(availableLanguages)
+
+    # Translate the recipe in all the other available languages
+    print("Translating recipe...")
     for language in availableLanguages:
-        if language == 'en':
+        # Skip English and Romanian
+        if language['language'] == 'en' or language['language'] == 'ro':
             continue
-        # print(availableLanguages[language])
-        folderPath = os.path.join(parentDir, availableLanguages[language])
-        # print(folderPath)
-        checkFolder(folderPath)
+        translateRecipe(recipe, originLanguage['name'], language['name'], originLanguage['language'], language['language'])
+        print("Translated to " + language['name'] + "!")
 
-__main__()
+    print("Finished translation!")
+
+
+main('1.txt', {'name': 'English', 'language': 'en'})
